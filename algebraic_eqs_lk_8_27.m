78fe9ae6a31182418475388fe865bbb4a3baf5ba
Libby Kirby <eakirby@uvm.edu>
2013-12-17 11:04:47 -0500

    Learning...

---
+++ algebraic_eqs_lk_8_27.m
@@ -0,0 +1,41 @@
+function [g,dg_dx,dg_dy] = algebraic_eqs(t,x,y,ps,opt) 
+% these give us the algebraic equations for the system
+
+global Load_spline
+
+C = psconstants_will;
+% extract some useful data about the system
+n         = size(ps.bus,1);
+ng        = size(ps.gen,1);
+mac_bus_i = ps.bus_i(ps.mac(:,1)); % machine bus indices
+Xd        = ps.mac(:,C.ma.Xd); % d axis machine reactance
+%Pd = ps.shunt(:,C.sh.P).*ps.shunt(:,C.sh.factor)./ps.baseMVA; % amount of demand
+D         = ps.bus_i(ps.shunt(:,1)); % locations of demand
+Pd        = [];
+for i=1:length(D)
+Pd (i)       = (ppval(Load_spline(i),t))/ps.baseMVA; %include stochastic load
+end
+
+
+% get some indices that allow us to keep track of stuff
+ix = get_indices_will(n,ng);
+% extract data from the input vectors
+theta = y(ix.y.theta); % bus angles
+delta = x(ix.x.delta); % machine rotor angles
+
+% calculate the electrical power produced by machines
+delta_mac = delta - theta(mac_bus_i); % the rotor angle, relative to the bus angle
+Pg = delta_mac./Xd; % the generator power, using the DC load flow assumptions
+
+% calculate the nodal power imbalance:
+g = ps.B*theta - sparse(mac_bus_i,1,Pg,n,1) + sparse(D,1,Pd,n,1);
+% power out through transmission lines - power in from gens + power out through loads = 0
+
+% find dg_dx
+if nargout>1
+    dg_dx = sparse(mac_bus_i,ix.x.delta,-1./Xd,ix.ng,ix.nx);
+end
+% find dg_dy
+if nargout>2
+    dg_dy = ps.B + sparse(mac_bus_i,ix.y.theta(mac_bus_i),+1./Xd,ix.ng,ix.ny);
+end
---
+++ differential_algebraic_eqs_lk_10_6.m
@@ -0,0 +1,29 @@
+function [fg,dfg_dxy] = differential_algebraic_eqs(t,xy,ps,opt)
+% this is the main set of DAEs that define out problem
+% usage: [fg,dfg_dxy] = differential_algebraic_eqs(t,xy,ps)
+
+if nargin<4
+    opt=[];
+end
+
+% extract some useful data about the system
+n  = size(ps.bus,1);
+ng = size(ps.gen,1);
+% some indices that allow us to keep track of stuff
+ix = get_indices_will(n,ng);
+% extract x and y from xy
+x = xy(1:ix.nx);
+y = xy((1:ix.ny)+ix.nx); 
+
+% calculate the derivatives if needed:
+if nargin==1 % just calculate the values
+    f = differential_eqs_lk_10_6(t,x,y,ps,opt);
+    g = algebraic_eqs_lk_8_27(t,x,y,ps,opt);
+    fg = [f;g];
+else % calculate the values and the derivatives
+    [f,df_dx,df_dy] = differential_eqs_lk_10_6(t,x,y,ps,opt);
+    [g,dg_dx,dg_dy] = algebraic_eqs_lk_8_27(t,x,y,ps,opt);
+    fg = [f;g];
+    dfg_dxy = [df_dx df_dy;
+               dg_dx dg_dy;];
+end
